dfc14da13c585488b8de95795a61d1f8695e6141
Arthur236 <arthur.thungu@gmail.com>
2017-11-13 17:03:56 +0300

    Make ajax request

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {fetchWeather} from "../actions/index";
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
     constructor(props) {
         super(props);
 
@@ -13,13 +16,15 @@ export default class SearchBar extends Component {
     }
 
     onInputChange(event) {
-        this.setState({term: event.target.value});
+        this.setState({ term: event.target.value });
     }
 
     onFormSubmit(event) {
         event.preventDefault();
 
-
+        // Fetch weather data
+        this.props.fetchWeather(this.state.term);
+        this.setState({ term: '' });
     }
 
     render() {
@@ -39,3 +44,9 @@ export default class SearchBar extends Component {
         );
     }
 }
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
